4ffc4e2fa23b1daa9c5af3a2de9001b1efc3fa77
cassiahahn <kklyra@gmail.com>
2022-02-08 11:08:33 +0100

    program install class

---
+++ program-install/first.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main ()
+{
+    printf("Ola mundo,ola de novo");
+    return 0;
+}
---
+++ program-install/second.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "Ola mundo" << "ola de novo";
+    return 0;
+}
